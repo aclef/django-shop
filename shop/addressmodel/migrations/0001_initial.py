67d73b6573b4d1f5ea26c0640f6749aaeea0bbf8
Eric Amador <eric.amador14@gmail.com>
2015-08-24 13:01:36 -0400

    Moving existing South migrations to south_migrations and generating new Django migrations.

--- shop/addressmodel/migrations/0001_initial.py
+++ shop/addressmodel/migrations/0001_initial.py
@@ -1,99 +1,57 @@
-# flake8: noqa
-# encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-
-        # Adding model 'Country'
-        db.create_table('addressmodel_country', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-        ))
-        db.send_create_signal('addressmodel', ['Country'])
-
-        # Adding model 'Address'
-        db.create_table('addressmodel_address', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('user_shipping', self.gf('django.db.models.fields.related.OneToOneField')(blank=True, related_name='shipping_address', unique=True, null=True, to=orm['auth.User'])),
-            ('user_billing', self.gf('django.db.models.fields.related.OneToOneField')(blank=True, related_name='billing_address', unique=True, null=True, to=orm['auth.User'])),
-            ('address', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('address2', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('zip_code', self.gf('django.db.models.fields.CharField')(max_length=20)),
-            ('city', self.gf('django.db.models.fields.CharField')(max_length=20)),
-            ('state', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('country', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['addressmodel.Country'], null=True, blank=True)),
-        ))
-        db.send_create_signal('addressmodel', ['Address'])
+from django.db import models, migrations
+from django.conf import settings
 
 
-    def backwards(self, orm):
+class Migration(migrations.Migration):
 
-        # Deleting model 'Country'
-        db.delete_table('addressmodel_country')
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-        # Deleting model 'Address'
-        db.delete_table('addressmodel_address')
-
-
-    models = {
-        'addressmodel.address': {
-            'Meta': {'object_name': 'Address'},
-            'address': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'address2': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'city': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
-            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['addressmodel.Country']", 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'state': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'user_billing': ('django.db.models.fields.related.OneToOneField', [], {'blank': 'True', 'related_name': "'billing_address'", 'unique': 'True', 'null': 'True', 'to': "orm['auth.User']"}),
-            'user_shipping': ('django.db.models.fields.related.OneToOneField', [], {'blank': 'True', 'related_name': "'shipping_address'", 'unique': 'True', 'null': 'True', 'to': "orm['auth.User']"}),
-            'zip_code': ('django.db.models.fields.CharField', [], {'max_length': '20'})
-        },
-        'addressmodel.country': {
-            'Meta': {'object_name': 'Country'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        },
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        }
-    }
-
-    complete_apps = ['addressmodel']
+    operations = [
+        migrations.CreateModel(
+            name='Address',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name='Name')),
+                ('address', models.CharField(max_length=255, verbose_name='Address')),
+                ('address2', models.CharField(max_length=255, verbose_name='Address2', blank=True)),
+                ('zip_code', models.CharField(max_length=20, verbose_name='Zip Code')),
+                ('city', models.CharField(max_length=20, verbose_name='City')),
+                ('state', models.CharField(max_length=255, verbose_name='State')),
+            ],
+            options={
+                'verbose_name': 'Address',
+                'verbose_name_plural': 'Addresses',
+            },
+        ),
+        migrations.CreateModel(
+            name='Country',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+            ],
+            options={
+                'verbose_name': 'Country',
+                'verbose_name_plural': 'Countries',
+            },
+        ),
+        migrations.AddField(
+            model_name='address',
+            name='country',
+            field=models.ForeignKey(verbose_name='Country', blank=True, to='addressmodel.Country', null=True),
+        ),
+        migrations.AddField(
+            model_name='address',
+            name='user_billing',
+            field=models.OneToOneField(related_name='billing_address', null=True, blank=True, to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='address',
+            name='user_shipping',
+            field=models.OneToOneField(related_name='shipping_address', null=True, blank=True, to=settings.AUTH_USER_MODEL),
+        ),
+    ]
